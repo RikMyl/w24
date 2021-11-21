1f49789f850e27b4bef009a930d2a6115c77ea88
timojkankaanpaa <tka@puv.fi>
2021-11-21 08:26:11 +0000

    Update Day.test.js

--- src/Day.test.js
+++ src/Day.test.js
@@ -4,12 +4,12 @@ import { configure, shallow, assert } from "enzyme";
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 import Day from "./Day";
 configure({ adapter: new Adapter() });
-describe('The business card data")', () => {
+describe('Testing Day-component")', () => {
   it("There is one div", () => {
     const wrapper = shallow(<Day month="10" />);
     expect(wrapper.find("div")).toHaveLength(1);
   });
-  it("There is one image", () => {
+  it("Test 7/2019", () => {
     const wrapper = shallow(<Day month="7" />);
     expect(wrapper.find("div").html()).toEqual(
       "<div>Month 7/2019 starts Monday</div>"
